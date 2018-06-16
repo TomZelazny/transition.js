9c2ed25e99870549fec8b0835282a8df82918783
TomZelazny <tom.zelazny@gmail.com>
2018-06-16 03:48:08 +0300

    remove example use from index.js

--- index.js
+++ index.js
@@ -24,15 +24,4 @@ class A{
 }
 
 // just to make syntax nicer
-function Animate(el){ return new A(el); }
-
-
-//USAGE
-let el = document.getElementById("test");
-let elAnimation = Animate(el);
-elAnimation
-    .animate("left", "500px", "ease", "5s", () => console.log("DONE1"))
-    .animate("top", "500px", "linear", "5s", () => console.log("DONE2"))
-    .animate("top", "0px", "ease-in", "5s", () => console.log("DONE3"))
-    .animate("transform", "rotate(360deg)", "linear", "3s", () => console.log("DONE4"))
-    .animate("left", "0px", "ease-out", "15s", (el) => el.parentNode.removeChild(el) );
+export function Animate(el){ return new A(el); }
